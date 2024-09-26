2a6a58829c6c56814b927d8307ee4004991b9138
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-09-26 14:49:39 +0200

    Change: document code and automate building and deploying docs

--- src/index.js
+++ src/index.js
@@ -54,11 +54,34 @@ class TestAdapter extends Adapter {
   }
 }
 
-export default class Helper {
+/**
+ * @callback botScript
+ * @async
+ * @param {Robot} robot
+ */
+
+/**
+ * Hubot Test Helper class
+ */
+class Helper {
+  /**
+   * Creates a Helper instance
+   *
+   * @param {...botScript} scripts - the script modules to load into the Bot
+   */
   constructor(...scripts) {
     this.scripts = scripts;
   }
 
+  /**
+   * Initializes the Bot and prepares everything needed to receive messages
+   *
+   * When providing `true` in `httpd`, {@link #destroy} must be called after the tests have completed.
+   *
+   * @async
+   * @param {boolean} [httpd=false] - Option to start an HTTP server for the Bot
+   * @returns {Promise<void>}
+   */
   async init(httpd = false) {
     this.robot = new Robot('TestAdapter', httpd, 'Hubot', 'hubot');
     this.robot.adapter = TestAdapter;
@@ -70,41 +93,103 @@ export default class Helper {
     this.robot.brain.emit('loaded');
   }
 
+  /**
+   * Destroys the Bot.
+   *
+   * Must be called when providing `true` in the `httpd` parameter when calling {@link #init}
+   *
+   * @async
+   * @returns {Promise<void>}
+   */
   async destroy() {
     if (this.robot.server) return new Promise((resolve) => this.robot.server.close(() => resolve(null)));
     else return Promise.resolve(null);
   }
 
+  /**
+   * Returns a new user that can interact with the Bot
+   *
+   * @param {string} name
+   * @param {string=} alias - defaults to the provided name in low case
+   * @returns {User}
+   */
   user(name, alias = null) {
     return this.robot.brain.userForId(alias || name.toLowerCase(), { name: name });
   }
 
+  /**
+   * Sends a `message` by a `user` in a `room`
+   *
+   * @async
+   * @param {User} user
+   * @param {string} room
+   * @param {string} message
+   * @returns {Promise<void>}
+   */
   async sendMessage(user, room, message) {
     await this.robot.adapter.say(user, room, message);
   }
 
+  /**
+   * Triggers an {@link EnterMessage} at the provided `room` by the provided `user`
+   *
+   * @async
+   * @param {User} user
+   * @param {string} room
+   * @returns {Promise<void>}
+   */
   async enter(user, room) {
     return this.robot.receive(new EnterMessage(userAt(user, room), false));
   }
 
+  /**
+   * Triggers a {@link LeaveMessage} at the provided `room` by the provided `user`
+   *
+   * @async
+   * @param {User} user
+   * @param {string} room
+   * @returns {Promise<void>}
+   */
   async leave(user, room) {
     return this.robot.receive(new LeaveMessage(userAt(user, room), false));
   }
 
-  emit(eventName, ...args) {
-    this.robot.emit(eventName, ...args);
+  /**
+   * Emits the provided `event` in the Bot
+   *
+   * @param {string} event
+   * @param {...string} args
+   * @returns null
+   */
+  emit(event, ...args) {
+    this.robot.emit(event, ...args);
   }
 
-  messagesAt(room, username) {
-    if (username === undefined) return this.robot.adapter.messagesAt(room);
-
-    return this.robot.adapter
-      .messagesAt(room)
-      .filter(([u]) => u === username)
-      .map(([, message]) => message);
-  }
-
+  /**
+   * Sets up a listener that will be called whenever the provided `event` is emitted by the Bot
+   *
+   * @param {string} event
+   * @param {function} listener
+   * @returns null
+   */
   on(event, listener) {
     this.robot.on(event, listener);
   }
+
+  /**
+   * Returns an array of arrays with:
+   * 0. The author's username
+   * 1. The message
+   *
+   * @param {string} room
+   * @param {string=} username - filter returned messages by author's username
+   * @returns {string[][]}
+   */
+  messagesAt(room, username) {
+    if (username === undefined) return this.robot.adapter.messagesAt(room);
+
+    return this.robot.adapter.messagesAt(room).filter(([u]) => u === username);
+  }
 }
+
+export default Helper;
